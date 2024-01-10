0cd7dc3503551cfb9bf4d9f2fbdb6ffca7291037
afshin-phpy <hassaniiafshin@gmail.com>
2024-01-10 04:08:48 +0330

    fix: test file bug fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,4 @@ def test_read_ram_info():
     response = client.get("/ram_info/1")
     assert response.status_code == 200
     data = response.json()
-    assert len(data[0]) == 5
+    assert len(data[0]) == 3
